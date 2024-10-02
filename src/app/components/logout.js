5f8701b499141f9d15fe2a74612ca48c5396faf3
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2024-10-02 09:55:45 -0400

    deleted logout component, not needed

--- src/app/components/logout.js
+++
@@ -1,7 +0,0 @@
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/server"
-
-export default async function Logout() {
-    return (
-        <LogoutLink className="text-kobWhite">Logout</LogoutLink>
-    )
-}
